9a1e48e25fbc39e74ce72e7a71bccbab09dada86
kelvin <76351213@qq.com>
2020-04-04 21:05:27 +0800

    chore: change gif

--- src/views/viewIndex/index.vue
+++ src/views/viewIndex/index.vue
@@ -1,26 +1,25 @@
 <!--
  * @Author: kelvin
  * @Date: 2019-11-24 10:24:05
- * @LastEditTime: 2020-03-28 17:59:53
+ * @LastEditTime: 2020-04-04 21:04:17
  * @FilePath: /demo/src/views/viewIndex/index.vue
  * @Description: 登陆
  -->
 <template>
   <div class="index">
-    <img alt="Vue logo"
-         src="./../../assets/logo.png">
+    <img alt="Vue logo" src="./../../assets/nishizhuma.gif" />
     <Index msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Index from '@/components/index.vue';
+import Index from "@/components/index.vue";
 
 export default {
-  name: 'index',
+  name: "index",
   components: {
-    Index,
-  },
+    Index
+  }
 };
 </script>
